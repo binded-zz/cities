24ac18be93e12f24637902a15e458e6bb9ff5202
binded-zz <binded2@gmail.com>
2026-03-28 04:38:22 -0400

    Add PersistentConfig class for settings management

---
+++ ModSettings.cs
@@ -0,0 +1,11 @@
+// Persistent Configuration Class
+
+public class PersistentConfig
+{
+    public string Setting1 { get; set; }
+    public int Setting2 { get; set; }
+
+    public PersistentConfig() {
+        // Default constructor
+    }
+}
