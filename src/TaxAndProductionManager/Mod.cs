301ab43fc117e9067cd6e49a8a224f6b29977077
binded-zz <binded2@gmail.com>
2026-03-28 04:34:37 -0400

    Add Mod.cs file

---
+++ src/TaxAndProductionManager/Mod.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TaxAndProductionManager
+{
+    public class Mod
+    {
+        public void Initialize()
+        {
+            // Initialization code
+        }
+    }
+}
