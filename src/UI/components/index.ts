8a898236748c5f0174859faad44881f6efbd6fba
binded-zz <binded2@gmail.com>
2026-03-28 18:52:42 -0400

    Create index.ts to export all UI components

---
+++ src/UI/components/index.ts
@@ -0,0 +1,4 @@
+export { default as TaxProductionWindow } from './TaxProductionWindow';
+export { default as TaxSlider } from './TaxSlider';
+export { default as ResourceRow } from './ResourceRow';
+export { default as ProductionDisplay } from './ProductionDisplay';
