8bee9f0ccc7ca9d98b89c202a1ee4e7ca81c4834
binded-zz <binded2@gmail.com>
2026-03-28 19:07:55 -0400

    Refactor ReactDOM rendering to use createRoot
    Updated ReactDOM rendering to use createRoot for better performance and compatibility with React 18.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-// CURRENT (DEPRECATED):
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
