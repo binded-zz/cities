bc83b9a55b3fe52c646777773d26803f2af9cdd7
binded-zz <binded2@gmail.com>
2026-03-28 18:53:53 -0400

    Create src/ModEntry.cs as main mod entry point

---
+++ src/ModEntry.cs
@@ -0,0 +1,14 @@
+// Main mod entry point for Cities Skylines 2 mod
+
+using System;
+
+namespace CitiesSkylines2Mod
+{
+    public class ModEntry
+    {
+        public void Start()
+        {
+            Console.WriteLine("Mod has started!");
+        }
+    }
+}
