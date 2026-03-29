f8ca2df98a6fcddc23924e86c5a3927d9e13c146
binded-zz <binded2@gmail.com>
2026-03-28 20:41:41 -0400

    Implement IMod interface in ModEntry.cs

--- src/ModEntry.cs
+++ src/ModEntry.cs
@@ -1,14 +1,27 @@
-// Main mod entry point for Cities Skylines 2 mod
-
-using System;
+using Colossal.Logging;
+using Game.Modding;
 
 namespace CitiesSkylines2Mod
 {
-    public class ModEntry
+    public class ModEntry : IMod
     {
-        public void Start()
+        public static readonly string Name = "CitiesSkylines2Mod";
+        private static ILog s_log = LogManager.GetLogger(Name);
+
+        public ILog log => s_log;
+
+        public void OnLoad(UpdateSystem updateSystem)
         {
-            Console.WriteLine("Mod has started!");
+            s_log.InfoFormat(LogChannel.Modding, "Loading {0}", Name);
+        }
+
+        public void OnDispose()
+        {
+            s_log.InfoFormat(LogChannel.Modding, "Disposing {0}", Name);
+        }
+
+        public void OnUpdate()
+        {
         }
     }
 }
