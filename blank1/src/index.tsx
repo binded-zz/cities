77bf4c3ff1ae15cf7f5ed6ef792a7e14759d50c0
aj <binded2@gmail.com>
2026-04-03 01:21:49 -0400

    chore: remove template/sample projects (blank1, ATPM) to clean workspace

--- blank1/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ModRegistrar } from "cs2/modding";
-import { HelloWorldComponent } from "mods/hello-world";
-
-const register: ModRegistrar = (moduleRegistry) => {
-
-    moduleRegistry.append('Menu', HelloWorldComponent);
-}
-
-export default register;
